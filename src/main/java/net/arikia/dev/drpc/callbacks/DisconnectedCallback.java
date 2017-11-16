18e1dac6a2348c4a30edcb9a8e5dca01e7fb3f2a
BuildTools <eyeware.vatuu@gmail.com>
2017-11-16 22:42:12 +0100

    Continued the documentation.

--- src/main/java/net/arikia/dev/drpc/callbacks/DisconnectedCallback.java
+++ src/main/java/net/arikia/dev/drpc/callbacks/DisconnectedCallback.java
@@ -13,8 +13,8 @@ public interface DisconnectedCallback extends Callback{
 
     /**
      * Method called when disconnected.
-     * @param errorCode Error Code
-     * @param message   Message
+     * @param errorCode Error code returned on disconnection.
+     * @param message   Message containing details about the disconnection.
      */
     void apply(int errorCode, String message);
 }
