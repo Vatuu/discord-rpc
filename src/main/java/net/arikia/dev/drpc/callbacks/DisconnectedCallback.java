0dd123879177c4413db51e2ce84da862c58c4b98
BuildTools <unconfigured@null.spigotmc.org>
2017-11-16 22:24:02 +0100

    Continued the documentation.

--- src/main/java/net/arikia/dev/drpc/callbacks/DisconnectedCallback.java
+++ src/main/java/net/arikia/dev/drpc/callbacks/DisconnectedCallback.java
@@ -2,6 +2,18 @@ package net.arikia.dev.drpc.callbacks;
 
 import com.sun.jna.Callback;
 
+/**
+ * @author Nicolas "Vatuu" Adamoglou
+ * @version 1.0
+ *
+ *
+ **/
 public interface DisconnectedCallback extends Callback{
+
+    /**
+     * Method called when disconnected.
+     * @param errorCode Error Code
+     * @param message   Message
+     */
     void apply(int errorCode, String message);
 }
