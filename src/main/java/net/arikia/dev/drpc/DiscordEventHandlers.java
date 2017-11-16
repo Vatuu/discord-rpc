998780e00addb7ace66d75047002d68faeda6c20
BuildTools <unconfigured@null.spigotmc.org>
2017-11-16 22:15:21 +0100

    Continued the documentation.

--- src/main/java/net/arikia/dev/drpc/DiscordEventHandlers.java
+++ src/main/java/net/arikia/dev/drpc/DiscordEventHandlers.java
@@ -3,38 +3,22 @@ package net.arikia.dev.drpc;
 import com.sun.jna.Structure;
 import net.arikia.dev.drpc.callbacks.*;
 
+/**
+ * @author Nicolas "Vatuu" Adamoglou
+ * @version 1.0
+ *
+ *
+ */
 public class DiscordEventHandlers extends Structure {
 
-    public static class ByValue extends DiscordEventHandlers implements Structure.ByValue {}
-
+    /**
+     * Callback called when Discord-RPC was initialized successfully.
+     */
     public ReadyCallback ready;
+
     public DisconnectedCallback disconnected;
     public ErroredCallback errored;
     public JoinGameCallback joinGame;
     public SpectateGameCallback spectateGame;
     public JoinRequestCallback joinRequest;
-
-    /*public static abstract class ReadyEvent implements Callback{
-        public abstract void apply();
-    }
-
-    public static abstract class DisconnectedEvent implements Callback{
-        public abstract void apply(int errorCode, String message);
-    }
-
-    public static abstract class ErroredEvent implements Callback{
-        public abstract void apply(int errorCode, String message);
-    }
-
-    public static abstract class JoinGameEvent implements Callback{
-        public abstract void apply(String joinSecret);
-    }
-
-    public static abstract class SpectateGameEvent implements Callback{
-        public abstract void apply(String spectateSecret);
-    }
-
-    public static abstract class JoinRequestEvent implements Callback{
-        public abstract void apply(DiscordJoinRequest request);
-    }*/
 }
