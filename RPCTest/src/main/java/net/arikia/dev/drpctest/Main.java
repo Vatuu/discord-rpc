981156780d9ee137f1169f21369d96c8197dd89b
DeJay Development <dejay@dejaydev.com>
2018-04-25 18:42:27 -0400

    Vatuu wanted to help

--- RPCTest/src/main/java/net/arikia/dev/drpctest/Main.java
+++ RPCTest/src/main/java/net/arikia/dev/drpctest/Main.java
@@ -3,82 +3,61 @@ package net.arikia.dev.drpctest;
 import net.arikia.dev.drpc.DiscordEventHandlers;
 import net.arikia.dev.drpc.DiscordRPC;
 import net.arikia.dev.drpc.DiscordRichPresence;
-import net.arikia.dev.drpc.callbacks.ReadyCallback;
 
 import javax.swing.*;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
-    public static boolean ready = false;
-
-    public static void main(String[] args){
-
-        Window w = new Window();
-
-        boolean running = true;
-
-        int score = 0;
+    public static void main(String[] args) {
 
         initDiscord();
 
-        while(running){
+        JFrame frame = new JFrame("Derp");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel text = new JLabel("In Discord, set your active game to: \"Derp\"");
+        frame.getContentPane().add(text, SwingConstants.CENTER);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+
+        int score = 0;
+
+        while (true) {
+            System.out.println("discordRunCallbacks...");
             DiscordRPC.discordRunCallbacks();
+            System.out.println("discordRunCallbacks - Success");
 
-            if(!ready)
-                continue;
+            System.out.println("getInput...");
+            System.out.print("> ");
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            System.out.println("getInput - Success");
 
-            String input = getInput();
-
-            if(input.equalsIgnoreCase("test")){
-                System.out.println("Test.");
-                score++;
-            }else if(input.equals("shutdown")){
-                running = false;
-            }else{
-                System.out.println("Unknown Command.");
+            if (!input.equalsIgnoreCase("shutdown")) {
+                if (input.equalsIgnoreCase("test")) {
+                    System.out.println("Test.");
+                    score++;
+                    DiscordRichPresence rich = new DiscordRichPresence();
+                    rich.state = "Running Test | Private";
+                    rich.details = "Score = " + score;
+                    DiscordRPC.discordUpdatePresence(rich);
+                } else {
+                    System.out.println("Unknown Command!");
+                }
+            } else {
+                // lol.
+                DiscordRPC.discordShutdown();
+                frame.dispose();
+                System.exit(0);
             }
-            DiscordRichPresence rich = new DiscordRichPresence();
-            rich.state = "Running Test | Private";
-            rich.details = "Score = " + score;
-            DiscordRPC.discordUpdatePresence(rich);
-            ready = true;
-        }
-
-        DiscordRPC.discordShutdown();
-        w.dispose();
-    }
-
-    static class Window extends JFrame{
-        Window(){
-            super("Derp");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(0, 0);
-            setVisible(true);
         }
     }
 
-    private static String getInput() {
-        System.out.print("> ");
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        return input.toLowerCase();
-    }
-
-    private static void initDiscord(){
+    private static void initDiscord() {
         DiscordEventHandlers handlers = new DiscordEventHandlers();
         handlers.ready = new Ready();
         DiscordRPC.discordInitialize("415885161457123338", handlers, true);
     }
 
-    private static class Ready implements ReadyCallback{
-        public void apply(){
-            System.out.println("Ready.");
-            DiscordRichPresence rich = new DiscordRichPresence();
-            rich.state = "Running Test | Private";
-            rich.details = "Score = 0";
-            DiscordRPC.discordUpdatePresence(rich);
-            ready = true;
-        }
-    }
 }
