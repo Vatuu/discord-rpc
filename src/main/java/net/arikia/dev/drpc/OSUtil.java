c3e11375e5b438a72b8be018baa6d4506ed9024e
DeJay <contact@dejaydev.com>
2018-07-28 19:19:05 -0500

    getProperty

--- src/main/java/net/arikia/dev/drpc/OSUtil.java
+++ src/main/java/net/arikia/dev/drpc/OSUtil.java
@@ -5,12 +5,12 @@ public final class OSUtil {
     //I wanted this to be easier and no stupid static.
 
     public static boolean isMac() {
-        return System.("os.name").toLowerCase()
+        return System.getProperty("os.name").toLowerCase()
                 .startsWith("mac");
     }
 
     public static boolean isWindows() {
-        return System.("os.name").toLowerCase()
+        return System.getProperty("os.name").toLowerCase()
                 .startsWith("win");
     }
 
