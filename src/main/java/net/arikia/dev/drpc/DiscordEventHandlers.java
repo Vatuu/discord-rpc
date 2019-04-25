c4c739472b8d7f07538baba12eb05cf78ea813f6
DeJay <contact@dejaydev.com>
2019-04-25 10:07:08 -0500

    Run Code Beautify

--- src/main/java/net/arikia/dev/drpc/DiscordEventHandlers.java
+++ src/main/java/net/arikia/dev/drpc/DiscordEventHandlers.java
@@ -9,87 +9,82 @@ import java.util.List;
 /**
  * @author Nicolas "Vatuu" Adamoglou
  * @version 1.5.1
- *
+ * <p>
  * Object containing references to all event handlers registered. No callbacks are necessary,
  * every event handler is optional. Non-assigned handlers are being ignored.
  */
 public class DiscordEventHandlers extends Structure {
 
-    @Override
-    public List<String> getFieldOrder(){
-        return Arrays.asList("ready", "disconnected", "errored", "joinGame", "spectateGame", "joinRequest");
-    }
+	/**
+	 * Callback called when Discord-RPC was initialized successfully.
+	 */
+	public ReadyCallback ready;
+	/**
+	 * Callback called when the Discord connection was disconnected.
+	 */
+	public DisconnectedCallback disconnected;
+	/**
+	 * Callback called when a Discord error occurred.
+	 */
+	public ErroredCallback errored;
+	/**
+	 * Callback called when the player joins the game.
+	 */
+	public JoinGameCallback joinGame;
+	/**
+	 * Callback called when the player spectates a game.
+	 */
+	public SpectateGameCallback spectateGame;
+	/**
+	 * Callback called when a join request is received.
+	 */
+	public JoinRequestCallback joinRequest;
 
-    /**
-     * Callback called when Discord-RPC was initialized successfully.
-     */
-    public ReadyCallback ready;
+	@Override
+	public List<String> getFieldOrder() {
+		return Arrays.asList("ready", "disconnected", "errored", "joinGame", "spectateGame", "joinRequest");
+	}
 
-    /**
-     * Callback called when the Discord connection was disconnected.
-     */
-    public DisconnectedCallback disconnected;
+	public static class Builder {
 
-    /**
-     * Callback called when a Discord error occurred.
-     */
-    public ErroredCallback errored;
+		DiscordEventHandlers h;
 
-    /**
-     * Callback called when the player joins the game.
-     */
-    public JoinGameCallback joinGame;
+		public Builder() {
+			h = new DiscordEventHandlers();
+		}
 
-    /**
-     * Callback called when the player spectates a game.
-     */
-    public SpectateGameCallback spectateGame;
+		public Builder setReadyEventHandler(ReadyCallback r) {
+			h.ready = r;
+			return this;
+		}
 
-    /**
-     * Callback called when a join request is received.
-     */
-    public JoinRequestCallback joinRequest;
+		public Builder setDisconnectedEventHandler(DisconnectedCallback d) {
+			h.disconnected = d;
+			return this;
+		}
 
-    public static class Builder {
+		public Builder setErroredEventHandler(ErroredCallback e) {
+			h.errored = e;
+			return this;
+		}
 
-        DiscordEventHandlers h;
+		public Builder setJoinGameEventHandler(JoinGameCallback j) {
+			h.joinGame = j;
+			return this;
+		}
 
-        public Builder(){
-            h = new DiscordEventHandlers();
-        }
+		public Builder setSpectateGameEventHandler(SpectateGameCallback s) {
+			h.spectateGame = s;
+			return this;
+		}
 
-        public Builder setReadyEventHandler(ReadyCallback r){
-            h.ready = r;
-            return this;
-        }
+		public Builder setJoinRequestEventHandler(JoinRequestCallback j) {
+			h.joinRequest = j;
+			return this;
+		}
 
-        public Builder setDisconnectedEventHandler(DisconnectedCallback d){
-            h.disconnected = d;
-            return this;
-        }
-
-        public Builder setErroredEventHandler(ErroredCallback e){
-            h.errored = e;
-            return this;
-        }
-
-        public Builder setJoinGameEventHandler(JoinGameCallback j){
-            h.joinGame = j;
-            return this;
-        }
-
-        public Builder setSpectateGameEventHandler(SpectateGameCallback s){
-            h.spectateGame = s;
-            return this;
-        }
-
-        public Builder setJoinRequestEventHandler(JoinRequestCallback j){
-            h.joinRequest = j;
-            return this;
-        }
-
-        public DiscordEventHandlers build() {
-            return h;
-        }
-    }
+		public DiscordEventHandlers build() {
+			return h;
+		}
+	}
 }
--- src/main/java/net/arikia/dev/drpc/DiscordUser.java
+++ src/main/java/net/arikia/dev/drpc/DiscordUser.java
@@ -8,34 +8,32 @@ import java.util.List;
 /**
  * @author Nicolas "Vatuu" Adamoglou
  * @version 1.5.1
- *
+ * <p>
  * Object containing information about a Discord user.
  */
 public class DiscordUser extends Structure {
 
-    @Override
-    public List<String> getFieldOrder(){
-        return Arrays.asList("userId", "username", "discriminator", "avatar");
-    }
+	/**
+	 * The userId of the player asking to join.
+	 */
+	public String userId;
+	/**
+	 * The username of the player asking to join.
+	 */
+	public String username;
+	/**
+	 * The discriminator of the player asking to join.
+	 */
+	public String discriminator;
+	/**
+	 * The avatar hash of the player asking to join.
+	 *
+	 * @see <a href="https://discordapp.com/developers/docs/reference#image-formatting">Image Formatting</a>
+	 */
+	public String avatar;
 
-    /**
-     * The userId of the player asking to join.
-     */
-    public String userId;
-
-    /**
-     * The username of the player asking to join.
-     */
-    public String username;
-
-    /**
-     * The discriminator of the player asking to join.
-     */
-    public String discriminator;
-
-    /**
-     * The avatar hash of the player asking to join.
-     * @see <a href="https://discordapp.com/developers/docs/reference#image-formatting">Image Formatting</a>
-     */
-    public String avatar;
+	@Override
+	public List<String> getFieldOrder() {
+		return Arrays.asList("userId", "username", "discriminator", "avatar");
+	}
 }
